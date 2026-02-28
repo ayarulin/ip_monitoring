7e64cddf4f4d9f99bdd2d9a590fbb805cdcaa224
Adil Yarulin <adil.jarulin@gmail.com>
2026-03-01 00:25:02 +0500

    Protect test suite from non-test DB

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,16 @@ require 'database_cleaner-sequel'
 require_relative '../lib/boot'
 require_relative '../lib/system/container'
 
+env = ENV['RUBY_ENV']
+
+abort "Refusing to run specs when RUBY_ENV=#{env.inspect} (expected 'test')" if env != 'test'
+
+db_url = ENV['DATABASE_URL']
+
+if db_url && !db_url.include?('_test')
+  abort "Refusing to run specs against non-test database (DATABASE_URL=#{db_url.inspect})"
+end
+
 DatabaseCleaner.allow_remote_database_url = true
 
 RSpec.configure do |config|
